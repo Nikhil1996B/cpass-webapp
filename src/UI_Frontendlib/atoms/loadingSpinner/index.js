a24e500c0f641ad5ab6dba7f34b32f0fdca62f1e
Nikhil Bhaskar <30387741+Nikhil1996B@users.noreply.github.com>
2021-01-05 10:51:50 +0530

    Added react lazy loading and loadingSpinner component

--- src/UI_Frontendlib/atoms/loadingSpinner/index.js
+++ src/UI_Frontendlib/atoms/loadingSpinner/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import loadingspiner from './loading-spinner.gif'
 import Loader from 'react-loader-spinner';
+
+require('./style.scss')
 export class LoadingSpinner extends Component {
     render() {
         return (
--- src/components/Router/routes.jsx
+++ src/components/Router/routes.jsx
@@ -1,51 +1,27 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { SignUpPage } from "../../containers/SignUpPage/SignUpPage";
 // import HomePage from "../../containers/HomePage/HomePage";
 import { ErrorPage } from "../../containers/ErrorPage/ErrorPage";
 import { VideoPage } from "../../containers/VideoPage/VideoPage";
 // import VideoInfoPage from "../../containers/VideoInfoPage/VideoInfoPage";
-import DynamicImport from '../../DynamicImports'
 import LoadingSpinner from '../../UI_Frontendlib/atoms/loadingSpinner'
 
-
-// Custom dynamic Imports
-const Home = (props) => (
-  <DynamicImport load={() => import('../../containers/HomePage/HomePage')}>
-    {
-      (Component) => Component === null
-        ? <LoadingSpinner />
-        : <Component {...props} />
-    }
-  </DynamicImport>
-)
-
-
-const VideoInfoPage = (props) => (
-  <DynamicImport load={() => import('../../containers/VideoInfoPage/VideoInfoPage')}>
-    {
-      (Component) => Component === null
-        ? <LoadingSpinner />
-        : <Component {...props} />
-    }
-  </DynamicImport>
-)
-
-// TODO - replace the dynamic code splitting to react splitting
-// Using react lazy laoding and react suspense
-// const LazyHome = React.lazy(() => import('../../containers/HomePage/HomePage'))
-// const LazyVideoInfo = React.lazy(() => import('../../containers/VideoInfoPage/VideoInfoPage'))
+const LazyHome = lazy(() => import('../../containers/HomePage/HomePage'))
+const LazyVideoInfo = lazy(() => import('../../containers/VideoInfoPage/VideoInfoPage'))
 
 function Routes() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/home" component={Home} />
-        <Route path="/player" component={VideoPage} />
-        <Route path="/videoinfo" component={VideoInfoPage} />
-        <Route component={ErrorPage} />
-      </Switch>
+      <Suspense fallback={<LoadingSpinner />}>
+        <Switch>
+          <Route exact path="/" component={LazyHome} />
+          <Route path="/home" component={LazyHome} />
+          <Route path="/player" component={VideoPage} />
+          <Route path="/videoinfo" component={LazyVideoInfo} />
+          <Route component={ErrorPage} />
+        </Switch>
+      </Suspense>
     </BrowserRouter>
   );
 }
